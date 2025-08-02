dd359dbffd2dbd293063fe1705492c588bcd393e
Piyush_Gupta <piyushg251004@gmail.com>
2025-08-02 14:40:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -247,6 +247,7 @@ async def global_exception_handler(request, exc):
     )
 
 if __name__ == "__main__":
+    import os
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
